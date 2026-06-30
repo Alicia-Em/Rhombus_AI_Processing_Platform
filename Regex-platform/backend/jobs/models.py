b74c715e7989c9c5a658eea60b577d0ef7046087
Alicia Emma Field <z5470333@ad.unsw.edu.au>
2026-07-01 09:42:46 +1000

    Created framework for job and bakcend

---
+++ Regex-platform/backend/jobs/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Jobs(models.Model):
+    STATUS_CHOICES = [
+        ("QUEUED", "Queued"),
+        ("RUNNING", "Running"),
+        ("SUCCESS" ,"Success"),
+        ("FAILED", "Failed"),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="QUEUED")
+    progress = models.IntegerField(default=0)
+    original_file = models.FileField(upload_to="uploads/")
+    result_file = models.FileField(upload_to="results/", null=True, blank=True)
+    natural_language_prompt = models.TextField()
+    regex_pattern = models.TextField(blank=True)
+    replacement_value = models.CharField(max_length=255)
+    target_column = models.CharField(max_length=255)
+    error_message = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
